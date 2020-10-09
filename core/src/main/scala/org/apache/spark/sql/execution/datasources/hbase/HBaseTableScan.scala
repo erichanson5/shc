bde630fe8b92193970fe41c90d8632e5de85bf79
Hanson <ehanson@tree.com>
2020-10-09 16:09:43 -0400

    shc-1.1.2-3.0-s_2.12

--- core/src/main/scala/org/apache/spark/sql/execution/datasources/hbase/HBaseTableScan.scala
+++ core/src/main/scala/org/apache/spark/sql/execution/datasources/hbase/HBaseTableScan.scala
@@ -22,8 +22,6 @@ package org.apache.spark.sql.execution.datasources.hbase
 
 import java.util.ArrayList
 
-import scala.collection.mutable
-
 import org.apache.hadoop.hbase.CellUtil
 import org.apache.hadoop.hbase.client._
 import org.apache.hadoop.hbase.filter.{Filter => HFilter}
@@ -33,9 +31,12 @@ import org.apache.spark.sql.Row
 import org.apache.spark.sql.execution.datasources.hbase
 import org.apache.spark.sql.execution.datasources.hbase.HBaseResources._
 import org.apache.spark.sql.sources.Filter
-import org.apache.spark.sql.types.StructType
+import org.apache.spark.sql.types._
 import org.apache.spark.util.ShutdownHookManager
-import org.apache.spark.sql.execution.datasources.hbase.types.SHCDataTypeFactory
+import org.apache.spark.sql.execution.datasources.hbase.types.{SHCDataType, SHCDataTypeFactory}
+
+import scala.collection.mutable
+import scala.util.matching.Regex
 
 private[hbase] case class HBaseRegion(
     override val index: Int,
@@ -86,6 +87,27 @@ private[hbase] class HBaseTableScanRDD(
     ps.asInstanceOf[Array[Partition]]
   }
 
+  private def extractKey(s: String, r: Regex): Option[String] = s match {
+    case r() => Some("")
+    case r(x) => Some(x)
+    case _ => None
+  }
+
+  private def containsDynamic(dt: DataType): Boolean = dt match {
+    case m: MapType => m.keyType.isInstanceOf[StringType.type]
+    case _ => false
+  }
+
+  private def getInternalValueType(dt: DataType): Option[DataType] = dt match {
+    case m: MapType => Some(m.valueType)
+    case _ => None
+  }
+
+  private def keepVersions(dt: DataType): Boolean = dt match {
+    case m: MapType => m.keyType.isInstanceOf[LongType.type]
+    case _ => false
+  }
+
   // TODO: It is a big performance overhead, as for each row, there is a hashmap lookup.
   def buildRow(fields: Seq[Field], result: Result): Row = {
     val r = result.getRow
@@ -99,21 +121,52 @@ private[hbase] class HBaseTableScanRDD(
       }
     }
 
-    val valueSeq = fields.filter(!_.isRowKey).map { x =>
-      val kv = result.getColumnLatestCell(
-        relation.catalog.shcTableCoder.toBytes(x.cf),
-        relation.catalog.shcTableCoder.toBytes(x.col))
-      if (kv == null || kv.getValueLength == 0) {
-        (x, null)
-      } else {
-        val v = CellUtil.cloneValue(kv)
-        (x, SHCDataTypeFactory.create(x).fromBytes(v))
-      }
-    }.toMap
+    import scala.collection.JavaConverters.mapAsScalaMapConverter
+    val scalaMap = result.getMap.asScala
 
-    val unioned = keySeq ++ valueSeq
+    val valuesSeq = scalaMap.flatMap { case (cf, columns) =>
+
+      val cfName = relation.catalog.shcTableCoder.fromBytes(cf).toString
+      val cfFields = fields.filter(_.cf == cfName)
+      val scalaColumns = columns.asScala
+
+      cfFields.map{ f =>
+        val dataType: SHCDataType = SHCDataTypeFactory.create(f)
+        if (f.col.isEmpty && containsDynamic(f.dt)) {
+
+          val m = scalaColumns.map { case (q, versions) =>
+            val pq = relation.catalog.shcTableCoder.fromBytes(q).toString
+            val v = if(getInternalValueType(f.dt).exists(keepVersions)) {
+              versions.asScala.mapValues(dataType.fromBytes)
+            } else {
+              dataType.fromBytes(versions.firstEntry().getValue)
+            }
+            pq -> v
+          }
+          cfFields.foreach(f => m.remove(f.col))
+          f -> m.toMap
+
+        } else {
+
+          val pq = relation.catalog.shcTableCoder.toBytes(f.col)
+          val timeseries = scalaColumns.get(pq)
+          val v = if(keepVersions(f.dt)) {
+            timeseries.map(_.asScala.mapValues(dataType.fromBytes))
+          } else {
+            timeseries.map(ver => dataType.fromBytes(ver.firstEntry().getValue))
+          }.getOrElse(null)
+          f -> v
+
+        }
+      }.toMap
+    }
+
+    val unioned = keySeq ++ valuesSeq
     // Return the row ordered by the requested order
-    Row.fromSeq(fields.map(unioned.get(_).getOrElse(null)))
+    val ordered = fields.map(unioned.getOrElse(_, null))
+    val additional = unioned.filterNot { case (f, _) => fields.contains(f) }.values
+    val unstructured = ordered ++ additional
+    Row.fromSeq(unstructured)
   }
 
   // TODO: It is a big performance overhead, as for each row, there is a hashmap lookup.
